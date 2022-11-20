30c14795847e8ffe3621ae76ba3da46accea775c
dastanchick <dosyafree32@gmail.com>
2022-11-20 19:46:52 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName( "Cancelo" );
+        person.setAge( -37 );
+        System.out.println( person );
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,40 @@
+class Person {
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        if (age < 0) {
+            this.age = null;
+        } else {
+            this.age = age;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
