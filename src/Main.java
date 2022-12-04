bd164a71d473e7ace6ac183aca17cf7e3496a109
dastanchick <dosyafree32@gmail.com>
2022-12-04 15:05:50 +0600

    I added number

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-    Scanner scanner = new Scanner(System.in);
-    int number = scanner.nextInt();
+    Scanner scanner = new Scanner(System.in);//todo sdfghjkdfghkjgfh
+    Random scanner1 = new Random();
+    int number = scanner1.nextInt(1000000);
+    int number2 = number;
     int percent = 0;
     while (number>0){
          number= number /10;
@@ -12,6 +15,8 @@ public class Main {
     }
         System.out.println("Loading...");
         TimeUnit.SECONDS.sleep(5);
+        System.out.println(number2);
         System.out.println(percent+" Орунду сан");
     }
 }
+
