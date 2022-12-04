a432a3188cc411c39b983b6c580c67f7d7622d39
dastanchick <dosyafree32@gmail.com>
2022-12-04 14:16:18 +0600

    I added number

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) {
-        Person person = new Person();
-        person.setName( "Cancelo" );
-        person.setAge( -37 );
-        System.out.println( person );
+    public static void main(String[] args) throws InterruptedException {
+    Scanner scanner = new Scanner(System.in);
+    int number = scanner.nextInt();
+    int percent = 0;
+    while (number>0){
+         number= number /10;
+        percent++;
+    }
+        System.out.println("Loading...");
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println(percent+" Орунду сан");
     }
 }
